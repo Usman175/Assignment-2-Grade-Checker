bd479622d46f3e4c3a11d9b0a332f71cf3a42dd6
usman175 <usmanali175@yahoo.com>
2017-03-08 22:36:50 +0500

    java assignment console.log()

---
+++ javascript assignment/app.js
@@ -0,0 +1,12 @@
+var a= +prompt("enter value");
+if(a>80){
+console.log("A Grade");
+}else if(a>70){
+console.log("B Grade");
+}else if(a>60){
+console.log("C Grade");
+}else if(a>50){
+console.log("D Grade");
+}else if(a<50){
+console.log("Fail");
+}
